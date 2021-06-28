5b85699bb4faa4d21eb035ad5727fb017a3190ae
cami670v <cami670v@ZBC-AA-UBUY12>
2021-06-28 09:06:19 +0200

    Renamed repo after detecting a spelling mistake

--- Sstopwatch/App.xaml.cs
+++ Sstopwatch/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Sstopwatch
+namespace Stopwatch
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Sstopwatch/MainWindow.xaml.cs
+++ Sstopwatch/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Sstopwatch
+namespace Stopwatch
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
